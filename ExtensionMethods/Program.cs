0866edc28ca254ae3c3896e5c1a3a1f4a9c5c57d
Volodymyr <volodymyr@mail.com>
2026-03-06 14:44:43 +0100

    Added extension methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionMethods
+{
+    internal class Program
+    {     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string");
+            string str = Console.ReadLine();
+
+           string res =  str.CleanString();
+
+            Console.WriteLine(res);
+        }
+    }
+}
