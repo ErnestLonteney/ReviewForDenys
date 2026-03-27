03be608c0b57e4137a9732e0c4850dfc5334615f
Denys Shaitan <schaitan.den@gmail.com>
2026-03-27 18:20:54 +0100

    Made a new code with yield break/return

---
+++ Yield/Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+
+namespace Yield
+{
+    public class Numbers
+    {
+        public static IEnumerable Number(int[] arr)
+        {
+            if (arr == null)
+            {
+                yield break;
+            }
+
+            foreach (int positive in arr)
+            {
+                if(positive < 0)
+                {
+                    yield return positive;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Yield/Program.cs
@@ -0,0 +1,14 @@
+﻿using Yield;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = {-10, 20, 30, -20, -40, 50, -90};
+
+        foreach (int i in Numbers.Number(array))
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
