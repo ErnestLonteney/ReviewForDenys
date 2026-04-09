1a7f8921ba94f006a5b096295192430430a2d0c0
Volodymyr <volodymyr@mail.com>
2026-04-09 19:35:32 +0200

    Generics

---
+++ Generics/Greeter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Greeter<T, K> where T : class where K : new() 
+    {
+        private readonly K[] values;
+
+        public Greeter(params K[] array)
+        {
+            K temp = new K();   
+
+            values = array;
+        }
+
+
+        public void Greet(T name)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Hello, {name}!");
+
+            Console.ResetColor();
+
+            foreach (K item in values)
+            {
+                var rnd = new Random();
+                int index = rnd.Next(0, 15);
+                Console.ForegroundColor = (ConsoleColor)index;
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
